8dbf35ea5b134090c1401db8a62c503641bef7db
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 06:31:14 +0700

    CT05: điều khiển LED2 chớp tắt

--- firmware/main.c
+++ firmware/main.c
@@ -1,9 +1,11 @@
-#include <REGX52.H>	
+#include <REGX52.H>
 sbit LED2 = P3^7;
-void main() {
+void main(){
 	unsigned long i;
-	LED2 = 0;	
-	for (i = 0; i < 22000; i++);
-	LED2 = 1;
-	while(1);
+	while (1){
+		LED2 = 1;
+		for (i = 0; i < 22000; i++);
+		LED2 = 0;
+		for (i = 0; i < 22000; i++);
+	}
 }
