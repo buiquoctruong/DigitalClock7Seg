5a545606c2d2948c1a7dd23068b5caba9a639918
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 16:41:18 +0700

    PHDTH13-CT01: hiển thị số trên 1 trên LED 7 đoạn thông qua giao tiếp I2C

--- firmware/main.c
+++ firmware/main.c
@@ -1,54 +1,100 @@
 #include <regx52.h>
-#include ".\ThuVien\Delay.h"
-#include ".\ThuVien\port.h"
-#include ".\ThuVien\dichNoiTiep.h"
-unsigned char code Code7Seg[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
+unsigned char maLED7[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
 
-void main(){	
-	unsigned char gio, phut, giay;
-	unsigned char chuc, dvi;
+#define DiaChiPCF8574 0x20
+sbit SCL = P1^0;
+sbit SDA = P1^1;
+
+void delay_short() {
 	unsigned char i;
-	gio = phut = giay = 0;
-	while(1){
-		for (i=0;i<166;i++){ //166
-			chuc = gio/10;
-			dvi = gio%10;
-			dichNoiTiep (Code7Seg[chuc]); 
-			led1 = 1;	Delay_ms(1);	
-			led1 = 0;
-			dichNoiTiep (Code7Seg[dvi]);
-			led2 = 1; Delay_ms(1);
-			led2 = 0;
-						
-			chuc = phut/10;
-			dvi = phut%10;
-			dichNoiTiep (Code7Seg[chuc]); 
-			led3 = 1; Delay_ms(1);
-			led3 = 0;			
-			dichNoiTiep (Code7Seg[dvi]); 
-			led4 = 1; Delay_ms(1);
-			led4 = 0;
-			
-			chuc = giay/10;
-			dvi = giay%10;
-			dichNoiTiep (Code7Seg[chuc]);
-			led5 = 1; Delay_ms(1);
-			led5 = 0;
-			dichNoiTiep (Code7Seg[dvi]);
-			led6 = 1; Delay_ms(1);
-			led6 = 0;
-		}
-		giay++;
-		if (giay == 60){
-			giay = 0;			
-			phut++;
-			if (phut == 60){
-				phut = 0;
-				gio++;
-				if (gio == 24){
-				gio = 0;
-				}
-			}
-		}
+	for (i = 0; i < 4; i++) {}
+}
+
+void delay(unsigned int time) {
+    TMOD = 0x01;        
+    while (time--) {
+        TH0 = -1000 / 256;
+        TL0 = -1000 % 256;
+        TR0 = 1;
+        while (!TF0);
+        TR0 = 0;
+        TF0 = 0;
+    }
+}
+
+void SCL_high() {
+	SCL = 1;
+	delay_short();
+}
+void SCL_low() {
+	SCL = 0; 
+	delay_short();
+}
+void I2C_Start() {
+	SDA = 1; SCL = 1;
+	SDA = 0;
+	delay_short();
+	SCL = 0; 
+	SDA = 1;
+}
+void I2C_Stop() {
+	SDA = 0; 
+	SCL_high();
+	SDA = 1;
+}
+
+bit I2C_Write(unsigned char dat) {
+	unsigned char i;
+	bit outbit;
+	for (i = 1; i <= 8; i++) {
+		outbit = dat & 0x80;
+		SDA = outbit;
+		dat = dat << 1;
+		SCL_high(); SCL_low();
+	}
+   // kiem tra ACK 
+	SDA = 1; 
+	SCL_high();
+	outbit = SDA; 
+	SCL_low();
+	return(outbit); 
+}
+
+unsigned char I2C_Read(bit ack) {
+    unsigned char i, dat;
+    bit inbit;
+    dat = 0;
+    for(i = 1; i <= 8; i++) {
+        SCL_high();
+        inbit = SDA;
+        dat = dat << 1;
+        dat = dat | inbit;
+        SCL_low();
+    }
+	// gui ACK/NACK
+    if (ack) 
+		SDA = 0; 
+    else 
+		SDA = 1;
+    SCL_high();
+    SCL = 0;
+    SDA = 1; 
+    delay_short();
+    return(dat);
+}
+
+void main() {
+	I2C_Start();
+	I2C_Write(DiaChiPCF8574 << 1);
+	I2C_Write(0xFF);
+	I2C_Stop();
+	while (1) {
+		I2C_Start();
+		I2C_Write(DiaChiPCF8574 << 1);
+		I2C_Write(maLED7[1]);
+		delay(1000);
+		I2C_Write(maLED7[2]);
+		delay(1000);
+		I2C_Stop();
 	}
 }
