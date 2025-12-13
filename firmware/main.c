2054340e2e001ea32c923a19d9048299ba85f74d
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 11:02:06 +0700

    CT05: điều khiển LED chớp/tắt bằng 2 nút nhấn Start và Stop

--- firmware/main.c
+++ firmware/main.c
@@ -1,22 +1,23 @@
 #include <REGX52.H>
 #include <.\ThuVien\Delay.h>
-sbit LED = P3^0;
-sbit ON = P1^0;
-sbit OFF = P1^1;
+#define LED_P0 P0
+#define LED_P3 P3
+sbit Start = P1^0;
+sbit Stop = P1^1;
 void main() {
-	P0 = P3 = 0;
-	LED = 0;
-	while (1) {
-		if (ON == 0) {
-			Delay_ms(20);
-			if (ON == 0) {
-				LED = 1;
-			}
-		}
-		if (OFF == 0) {
-			Delay_ms(20);
-			if (OFF== 0) {
-				LED = 0;
+	LED_P0 = 0xFF;
+	LED_P3 = 0xFF;
+	while(1) {
+		if (Start == 0){
+			while(1) {
+				LED_P0 = ~LED_P0;
+				LED_P3 = ~LED_P3;
+				Delay_ms(500);
+				if (Stop == 0){
+					LED_P0 = 0x00;
+					LED_P3 = 0x00;
+					break;
+				}
 			}
 		}
 	}
