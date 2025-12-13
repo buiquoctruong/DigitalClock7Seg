1bc41a5bbe5fa458a63fe84ff57f0f4ad84d9ffe
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 12:58:00 +0700

    CT01: Hiển thị 1 số trên LED 7 đoạn bằng cách điều khiển từng chân của Port 2 hoặc cả Port 2.

--- firmware/main.c
+++ firmware/main.c
@@ -1,21 +1,17 @@
-#include <REGX52.H>
+#include <regx52.h>
 #include ".\ThuVien\Delay.h"
-void main (){
-	unsigned char i;
-	unsigned int t;
-	while (1){
-		t = 500;
-		for (i = 0; i < 5; i++) {
-			P0 = 0x00; Delay_ms(t);
-			P0 = 0xFF; Delay_ms(t);
-		}
-		for (i = 0; i < 5; i++) {
-			P0 = 0x0F; Delay_ms(t);
-			P0 = 0xF0; Delay_ms(t);
-		}
-		for (i=0; i < 5;i++) {
-			P0 = 0x55; Delay_ms(t);
-			P0 = 0xAA; Delay_ms(t);
-		}
+void main(){
+	while(1){
+	P2_0 = 1;	
+	P2_1 = 0;
+	P2_2 = 0;
+	P2_3 = 1;
+	P2_4 = 1;
+	P2_5 = 1;
+	P2_6 = 1;
+	P2_7 = 1;
+	Delay_ms(1000);
+	P2 = 0xA4;
+	Delay_ms(1000);
 	}
 }
