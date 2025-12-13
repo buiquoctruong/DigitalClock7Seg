ad4605aa0bcf1c55d94435e2fcfb7621fef315b0
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 13:01:10 +0700

    CT03: Hiển thị LED 7 đoạn đếm từ 0 đến 9

--- firmware/main.c
+++ firmware/main.c
@@ -2,10 +2,11 @@
 #include ".\ThuVien\Delay.h"
 unsigned char code Code7Seg[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
 void main(){
+	unsigned char i;
 	while(1){
-		P2 = Code7Seg[3];
-		Delay_ms(1000);
-		P2 = Code7Seg[4];
-		Delay_ms(1000);
+		for (i=0; i<10; i++){
+			P2 = Code7Seg[i];
+			Delay_ms(1000);
+		}
 	}
 }
