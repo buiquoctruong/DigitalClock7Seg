95b7f11b86860f834fbe5c70efe85e0140a0d5aa
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 11:00:56 +0700

    CT04: điều khiển LED sáng/tắt bằng 2 nút nhấn ON và OFF

--- firmware/main.c
+++ firmware/main.c
@@ -1,16 +1,22 @@
 #include <REGX52.H>
 #include <.\ThuVien\Delay.h>
 sbit LED = P3^0;
-sbit NutNhanB1 = P1^0;
+sbit ON = P1^0;
+sbit OFF = P1^1;
 void main() {
 	P0 = P3 = 0;
 	LED = 0;
-	while(1) {
-		if (NutNhanB1 == 0) {
+	while (1) {
+		if (ON == 0) {
 			Delay_ms(20);
-			if (NutNhanB1 == 0) {
-			LED = !LED;
-			while (NutNhanB1 == 0); 
+			if (ON == 0) {
+				LED = 1;
+			}
+		}
+		if (OFF == 0) {
+			Delay_ms(20);
+			if (OFF== 0) {
+				LED = 0;
 			}
 		}
 	}
