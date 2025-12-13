51237b7bac69c69dceb84918310e0f404e067813
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 13:04:09 +0700

    CT06: đèn giao thông đỏ 20s, xanh 15s, vàng 5s.

--- firmware/main.c
+++ firmware/main.c
@@ -1,16 +1,34 @@
-#include <regx52.h>
+#include <REGX52.H>
 #include ".\ThuVien\Delay.h"
 unsigned char code Code7Seg[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
 #define LED7Chuc P2
 #define LED7Dvi P3
 sbit LEDDo = P0^0;
-sbit LED_VANG = P0^1;
-sbit LED_XANH = P0^2;
+sbit LEDVang = P0^1;
+sbit LEDXanh = P0^2;
 void main(){
 	signed char i;
 	while(1){
-		LEDDo = 0;	
-		for (i = 20; i >= 0; i--){
+		LEDDo = 0;
+		LEDXanh = 1;
+		LEDVang = 1;
+		for (i=20; i>=0; i--){ 
+			LED7Chuc = Code7Seg[i/10];
+			LED7Dvi = Code7Seg[i%10];
+			Delay_ms(1000);
+		}
+		LEDDo = 1;
+		LEDXanh = 0;
+		LEDVang = 1;
+		for (i=15; i>=0; i--){
+			LED7Chuc = Code7Seg[i/10];
+			LED7Dvi = Code7Seg[i%10];
+			Delay_ms(1000);
+		}
+		LEDDo = 1;
+		LEDXanh = 1;
+		LEDVang = 0;
+		for (i=5; i>=0; i--){
 			LED7Chuc = Code7Seg[i/10];
 			LED7Dvi = Code7Seg[i%10];
 			Delay_ms(1000);
