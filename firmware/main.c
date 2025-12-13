ae1e9487fe80fee3cfd42649caf86aca220b66c3
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 10:57:26 +0700

    CT01: nhấn giữ nút B1 thì LED sáng, ngược lại LED tắt

--- firmware/main.c
+++ firmware/main.c
@@ -1,21 +1,15 @@
 #include <REGX52.H>
-#include ".\ThuVien\Delay.h"
-void main (){
-	unsigned char i;
-	unsigned int t;
+sbit LED = P3^0;
+sbit NutNhanB1 = P1^0;
+void main() {
+	P0 = P3 = 0;
+	LED = 0; 	
 	while (1){
-		t = 500;
-		for (i = 0; i < 5; i++) {
-			P0 = 0x00; Delay_ms(t);
-			P0 = 0xFF; Delay_ms(t);
+		if ( NutNhanB1 == 0) {	
+			LED = 1; 	
 		}
-		for (i = 0; i < 5; i++) {
-			P0 = 0x0F; Delay_ms(t);
-			P0 = 0xF0; Delay_ms(t);
-		}
-		for (i=0; i < 5;i++) {
-			P0 = 0x55; Delay_ms(t);
-			P0 = 0xAA; Delay_ms(t);
+		else {		
+		 	LED = 0;
 		}
 	}
 }
