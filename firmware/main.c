622936c46acec397c83666572228aa4b1c5ee403
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 15:24:08 +0700

    CT02:  đếm từ 0 đến 9

--- firmware/main.c
+++ firmware/main.c
@@ -2,29 +2,28 @@
 #include ".\ThuVien\Delay.h"
 unsigned char code Code7Seg[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
 
-// Khai bao ket noi
 sbit DS = P3^0;
 sbit SHCP = P3^3;	
 sbit STCP = P3^2;	
-
-unsigned char Data; 
-unsigned char i;	
-unsigned char MatNaBit; 
-unsigned char GiaTriBit; 
-
-void main(){
-	Data = Code7Seg[2];
-	P0 = Data;		
+	
+void DichNoiTiep (unsigned char Data){
+	unsigned char i;	
 	for (i=0; i<8; i++){
-		MatNaBit = 0x80>>i;	
-		GiaTriBit = Data&MatNaBit;
-		DS = GiaTriBit ? 1 : 0; 
-		// Xung dich du lieu
-		SHCP = 0;
-		SHCP = 1;
+		DS = Data&(0x80>>i) ? 1 : 0; 
+		SHCP = 0; SHCP = 1;
 	}
-	// xung chot du lieu
-	STCP = 0;
-	STCP = 1;
-	while(1);
+	STCP = 0; STCP = 1;
+}
+
+void main(){	
+	unsigned char so=0;
+	while(1){
+		P0 = Code7Seg[so];
+		DichNoiTiep (Code7Seg[so]);
+		Delay_ms(500);
+		so++;
+		if (so == 9){
+			so = 0;	
+		}
+	}
 }
