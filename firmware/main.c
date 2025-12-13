cd62d60b1e84d59ee8be8c3e6194879c0e0396eb
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 10:50:16 +0700

    CT01: điều khiển chân P0^0 nhấp nháy sử dụng lệnh đảo bit

--- firmware/main.c
+++ firmware/main.c
@@ -1,21 +1,12 @@
 #include <REGX52.H>
-#include ".\ThuVien\Delay.h"
-void main (){
-	unsigned char i;
-	unsigned int t;
-	while (1){
-		t = 500;
-		for (i = 0; i < 5; i++) {
-			P0 = 0x00; Delay_ms(t);
-			P0 = 0xFF; Delay_ms(t);
-		}
-		for (i = 0; i < 5; i++) {
-			P0 = 0x0F; Delay_ms(t);
-			P0 = 0xF0; Delay_ms(t);
-		}
-		for (i=0; i < 5;i++) {
-			P0 = 0x55; Delay_ms(t);
-			P0 = 0xAA; Delay_ms(t);
-		}
+#include <.\ThuVien\Delay.h>
+sbit LED_PIN = P0^0;
+void main () {
+	P0 = 0;
+	P3 = 0;
+	LED_PIN = 0;
+	while(1){
+		LED_PIN = !LED_PIN;
+		Delay_ms(500);
 	}
 }
