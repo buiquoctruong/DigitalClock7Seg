3a1b0809cb6b65a1b2e1292d8d8066eebaa927b6
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 13:18:12 +0700

    CT01: hiển thị số gồm 2 chữ số hiển thị trên 2 LED 7 đoạn

--- firmware/main.c
+++ firmware/main.c
@@ -1,21 +1,24 @@
-#include <REGX52.H>
+#include <regx52.h>
 #include ".\ThuVien\Delay.h"
-void main (){
-	unsigned char i;
-	unsigned int t;
-	while (1){
-		t = 500;
-		for (i = 0; i < 5; i++) {
-			P0 = 0x00; Delay_ms(t);
-			P0 = 0xFF; Delay_ms(t);
-		}
-		for (i = 0; i < 5; i++) {
-			P0 = 0x0F; Delay_ms(t);
-			P0 = 0xF0; Delay_ms(t);
-		}
-		for (i=0; i < 5;i++) {
-			P0 = 0x55; Delay_ms(t);
-			P0 = 0xAA; Delay_ms(t);
-		}
+unsigned char code maLED7[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
+unsigned char dem;
+unsigned char chuc, dvi;
+#define abcdefg P3
+sbit led1 = P1^2;
+sbit led2 = P1^3;
+void main(){
+	P1 = 0;
+	dem = 23;
+	chuc = dem/10;
+	dvi = dem%10;
+	while(1){
+		abcdefg = maLED7[chuc]; 
+		led1 = 1;
+		Delay_ms(2);	
+		led1 = 0;	
+		abcdefg = maLED7[dvi]; 
+		led2 = 1;
+		Delay_ms(2);
+		led2 = 0;
 	}
 }
