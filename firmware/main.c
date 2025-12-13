8320a9c6c4de597f9dc97eecf0d9c9d1d2cf3743
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 11:04:26 +0700

    CT07: chương trình con Delay1SvaQuetPhim()

--- firmware/main.c
+++ firmware/main.c
@@ -4,25 +4,30 @@
 #define LED_P3 P3
 sbit Start = P1^0;
 sbit Stop = P1^1;
-void main(){
+//	
+unsigned char Delay1SvaQuetPhim(){
 	unsigned char i;
+	for (i=0; i<100; i++) {
+		Delay_ms(10);			
+		if (Stop == 0) {
+			LED_P0 = 0x00;
+			LED_P3 = 0x00;
+			return 1;
+		}
+	}
+	return 0;
+}
+//	
+void main() {
 	LED_P0 = 0x00;
 	LED_P3 = 0x00;
-	while(1){ 	//while(1)_1
-		if (Start == 0){
-			while(1){	//while(1)_2
+	while(1) {	// while(1)_1
+		if (Start == 0) {	// if_1
+			while(1) {	// while(1)_2
 				LED_P0 = ~LED_P0;
 				LED_P3 = ~LED_P3;
-				for (i=0; i<100; i++){
-					Delay_ms(10);
-					if (Stop == 0){
-						LED_P0 = 0x00;
-						LED_P3 = 0x00;
-						break;	//break_1
-					}
-				}
-				if (i != 100){
-					break; 	//break_2
+				if (Delay1SvaQuetPhim() ){	// if_2
+					break;
 				}
 			}
 		}
