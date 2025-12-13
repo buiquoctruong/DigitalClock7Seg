87452f2a6fda9e4f349d2513b059b58f945b5e11
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 16:12:23 +0700

    CT04: đồng hồ số sử dụng dịch dữ liệu nối tiếp 74HC595 Hiệu chỉnh port.h lần 1

--- firmware/ThuVien/port.h
+++ firmware/ThuVien/port.h
@@ -4,4 +4,11 @@
 sbit DS = P3^0;
 sbit SHCP = P3^3;	
 sbit STCP = P3^2;
-#endif
+// VDK voi bat/tat LED 7 doan
+sbit led1 = P1^2;
+sbit led2 = P1^3;
+sbit led3 = P1^4;
+sbit led4 = P1^5;
+sbit led5	= P1^6;
+sbit led6 = P1^7;
+#endif	
--- firmware/main.c
+++ firmware/main.c
@@ -4,29 +4,51 @@
 #include ".\ThuVien\dichNoiTiep.h"
 unsigned char code Code7Seg[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
 
-sbit Chuc	= P1^2;
-sbit Dvi 	= P1^3;
-
 void main(){	
+	unsigned char gio, phut, giay;
 	unsigned char chuc, dvi;
-	unsigned char i, so;
-	P1 = 0;
+	unsigned char i;
+	gio = phut = giay = 0;
 	while(1){
-		for (i=0;i<166;i++){ 
-			chuc = so/10;
-			dvi = so%10;
-			
+		for (i=0;i<166;i++){ //166
+			chuc = gio/10;
+			dvi = gio%10;
 			dichNoiTiep (Code7Seg[chuc]); 
-			Chuc = 1; Delay_ms(2);	
-			Chuc = 0;
+			led1 = 1;	Delay_ms(1);	
+			led1 = 0;
 			dichNoiTiep (Code7Seg[dvi]);
-			Dvi = 1; Delay_ms(2);
-			Dvi = 0;
+			led2 = 1; Delay_ms(1);
+			led2 = 0;
+						
+			chuc = phut/10;
+			dvi = phut%10;
+			dichNoiTiep (Code7Seg[chuc]); 
+			led3 = 1; Delay_ms(1);
+			led3 = 0;			
+			dichNoiTiep (Code7Seg[dvi]); 
+			led4 = 1; Delay_ms(1);
+			led4 = 0;
+			
+			chuc = giay/10;
+			dvi = giay%10;
+			dichNoiTiep (Code7Seg[chuc]);
+			led5 = 1; Delay_ms(1);
+			led5 = 0;
+			dichNoiTiep (Code7Seg[dvi]);
+			led6 = 1; Delay_ms(1);
+			led6 = 0;
 		}
-
-		so++;
-		if (so == 60){
-			so = 0;	
+		giay++;
+		if (giay == 60){
+			giay = 0;			
+			phut++;
+			if (phut == 60){
+				phut = 0;
+				gio++;
+				if (gio == 24){
+				gio = 0;
+				}
+			}
 		}
 	}
 }
