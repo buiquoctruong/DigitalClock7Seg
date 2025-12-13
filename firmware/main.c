e6fcff1c8c357c4a509abfa60c11ea407eaf5261
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 09:36:16 +0700

    - CT03: tạo chương trình con Delay_ms

--- firmware/main.c
+++ firmware/main.c
@@ -1,15 +1,21 @@
-#include <REGX52.H>
+#include <REGX52.H>	
+//		
+void Delay_ms (unsigned int t) {
+	unsigned int x, y;
+	for (x = 0; x < t; x++){
+		for (y = 0; y < 123; y++);
+	}
+}
+//	
 void main(){
-	unsigned long i;
-	P0 = 0x00;
 	while (1){
 		P0 = 0x0F;
-		for (i = 0; i < 22000; i++);
+		Delay_ms (500);
 		P0 = 0xF0;
-		for (i = 0; i < 22000; i++);
+		Delay_ms (500);
 		P0 = 0x55;
-		for (i = 0; i < 22000; i++);
+		Delay_ms (500);
 		P0 = 0xAA;
-		for (i = 0; i < 22000; i++);
+		Delay_ms (500);
 	}
 }
