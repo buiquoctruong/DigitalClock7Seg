0d74cb11f159601086d902ac415bc7b24b2c9f42
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 13:09:16 +0700

    CT02: Hiển thị hàng đơn vị và hàng chục

--- firmware/main.c
+++ firmware/main.c
@@ -1,10 +1,13 @@
 #include <regx52.h>
-unsigned char dem = 0;
-unsigned char dvi = 0;
-void main(){	
+unsigned char dem, tam = 0;
+unsigned char dvi, chuc = 0;
+void main(){
 	dem = 234;
 	dvi = dem%10;
-	P2 = dvi;				
+	chuc = dem%100;			chuc = chuc/10;
+	tam = chuc << 4;		
+	tam |= dvi;
+	P2 = tam;
 	while(1){
 	}
 }
