bc58d0a8b6faab13220eeed110ebf57e15d306d5
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 16:58:35 +0700

    PHDT13-CT03: điều khiển nút nhấn đếm lên hiển thị trênLED 7 đoạn

--- firmware/main.c
+++ firmware/main.c
@@ -3,22 +3,47 @@
 #include ".\ThuVien\port.h"
 #include ".\ThuVien\I2C.h"
 
-unsigned char maLED7[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
-#define DiaChiPCF8574 0x20
+unsigned char maLED7[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
 
-void HienThiLED7(unsigned char so) {
+#define DiaChiPCF8574_1 0x20
+#define DiaChiPCF8574_2 0x21
+
+void HienThiLED7 (unsigned char so) {
     I2C_Start();
-    I2C_Write(DiaChiPCF8574 << 1);
+    I2C_Write(DiaChiPCF8574_1 << 1);
     I2C_Write(maLED7[so]);
     I2C_Stop();
 }
 
+unsigned char DocNutNhan() {
+    unsigned char trangThaiNut;
+    I2C_Start();
+    I2C_Write (DiaChiPCF8574_2 << 1 | 1);
+    trangThaiNut = I2C_Read(1);
+    I2C_Stop();
+    return trangThaiNut;
+}
+
 void main() {
+    unsigned char trangThaiNut;
     unsigned char so = 0;
+    bit daNhan = 0;
+    I2C_Start();
+    I2C_Write (DiaChiPCF8574_1 << 1);
+    I2C_Write(0xFF);
+    I2C_Stop();
+    HienThiLED7(so);
     while (1) {
-        HienThiLED7(so);
-        so++;
-        if (so > 9) so = 0;
-        Delay_ms(500);
+        trangThaiNut = DocNutNhan();
+        if ((trangThaiNut & 0x01) == 0 && daNhan == 0) {
+            so++;
+            if (so > 9) so = 0;
+            HienThiLED7(so);
+            daNhan = 1;
+        }
+        if ((trangThaiNut & 0x01) == 1) {
+            daNhan = 0;
+        }
+        Delay_ms(100);
     }
 }
