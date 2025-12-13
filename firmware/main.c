78d7ae9bf720d4b3d1ccfc48ed4d23b52e47cbcd
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 10:52:17 +0700

    CT03: dịch 1 bit từ trái sang phải từ P0^0 đến P3^7.

--- firmware/main.c
+++ firmware/main.c
@@ -2,13 +2,23 @@
 #include <.\ThuVien\Delay.h>
 #define LED_PORT0 P0
 #define LED_PORT3 P3
+unsigned char i;
 void main(){
-	LED_PORT0 = 0x01;
-	LED_PORT3 = 0x80;
+	LED_PORT0 = 0x00;
+	LED_PORT3 = 0x00; 
 	Delay_ms(500);
 	while(1){	
-		LED_PORT0 = LED_PORT0 << 1;
-		LED_PORT3 = LED_PORT3 >> 1;
+		LED_PORT0 = 0x01;
 		Delay_ms(500);
+		for(i=0; i<8; i++){
+			LED_PORT0 = LED_PORT0 << 1;
+			Delay_ms(500);
+		}
+		LED_PORT3 = 0x01;
+		Delay_ms(500);
+		for(i=0; i<8; i++){
+			LED_PORT3 = LED_PORT3 << 1;
+			Delay_ms(500);
+		}
 	}
 }
