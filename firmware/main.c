78c786692d7f7bf4336b742874e8e1d513d889de
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 13:11:26 +0700

    CT03: Hiển thị hàng đơn vị, chục, trăm

--- firmware/main.c
+++ firmware/main.c
@@ -1,13 +1,17 @@
 #include <regx52.h>
-unsigned char dem, tam = 0;
-unsigned char dvi, chuc = 0;
+unsigned char dem, tam;
+unsigned char dvi, chuc; 
+unsigned char tram;
 void main(){
 	dem = 234;
-	dvi = dem%10;
-	chuc = dem%100;			chuc = chuc/10;
-	tam = chuc << 4;		
+	dvi = dem%10;	
+	chuc = dem%100;
+	chuc = chuc/10;
+	tam = chuc << 4;
 	tam |= dvi;
 	P2 = tam;
+	tram = dem/100;
+	P3 = tram;
 	while(1){
 	}
 }
