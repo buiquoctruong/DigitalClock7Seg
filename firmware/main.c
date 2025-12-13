ed8d104c7613603f13a9a4ad13e2f6665adabf92
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 09:34:57 +0700

    - CT02: tạo các hiệu ứng cho LED

--- firmware/main.c
+++ firmware/main.c
@@ -1,4 +1,15 @@
 #include <REGX52.H>
-void main ( ){	
-	P0 = 0xFF;
+void main(){
+	unsigned long i;
+	P0 = 0x00;
+	while (1){
+		P0 = 0x0F;
+		for (i = 0; i < 22000; i++);
+		P0 = 0xF0;
+		for (i = 0; i < 22000; i++);
+		P0 = 0x55;
+		for (i = 0; i < 22000; i++);
+		P0 = 0xAA;
+		for (i = 0; i < 22000; i++);
+	}
 }
