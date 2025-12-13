d46c17098bb0a10946a38d6f59652b6a3802ea3f
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 10:39:22 +0700

    CT04: hiệu ứng Led sáng lan dần từ trái sang phải và ngược lại

--- firmware/main.c
+++ firmware/main.c
@@ -1,18 +1,32 @@
 #include <REGX52.H>
 #include ".\ThuVien\Delay.h"	
 #define LED_PORT P0
+unsigned char i;
+//	
+void LEDSangLanDanTraiPhai (){
+	unsigned char M[ ] = {0xFE, 0xFC, 0xF8, 0xF0, 0xE0, 0xC0, 0x80, 0x00};
+	LED_PORT = 0xFF;	
+	Delay_ms(100);
+	for (i = 0; i < 8; i++) {
+    	LED_PORT = M[i];
+		Delay_ms(100);
+  }
+}
+
+//	
+void LEDSangLanDanPhaiTrai (){
+	unsigned char N[ ] = {0x7F, 0x3F, 0x1F, 0x0F, 0x07, 0x03, 0x01, 0x00};
+	LED_PORT = 0xFF;	
+	Delay_ms(100);
+	for (i = 0; i < 8; i++) {
+    LED_PORT = N[i];	
+		Delay_ms(100);
+  }
+}
+
 void main(){
-		unsigned int t;
-		while (1){
-		t = 500;
-		LED_PORT = 0xFF;	Delay_ms(t);
-		LED_PORT = 0xFE; 	Delay_ms(t);
-		LED_PORT = 0xFC; 	Delay_ms(t);
-		LED_PORT = 0xF8; 	Delay_ms(t);
-		LED_PORT = 0xF0; 	Delay_ms(t);
-		LED_PORT = 0xE0; 	Delay_ms(t);
-		LED_PORT = 0xC0; 	Delay_ms(t);
-		LED_PORT = 0x80; 	Delay_ms(t);
-		LED_PORT = 0x00; 	Delay_ms(t);
+	while(1){
+		LEDSangLanDanTraiPhai();
+		LEDSangLanDanPhaiTrai();
 	}
 }
