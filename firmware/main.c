88364fe58336b35a8b54e20ac554dc86983799c8
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 13:08:30 +0700

    CT01: Hiển thị hàng đơn vị

--- firmware/main.c
+++ firmware/main.c
@@ -1,21 +1,10 @@
-#include <REGX52.H>
-#include ".\ThuVien\Delay.h"
-void main (){
-	unsigned char i;
-	unsigned int t;
-	while (1){
-		t = 500;
-		for (i = 0; i < 5; i++) {
-			P0 = 0x00; Delay_ms(t);
-			P0 = 0xFF; Delay_ms(t);
-		}
-		for (i = 0; i < 5; i++) {
-			P0 = 0x0F; Delay_ms(t);
-			P0 = 0xF0; Delay_ms(t);
-		}
-		for (i=0; i < 5;i++) {
-			P0 = 0x55; Delay_ms(t);
-			P0 = 0xAA; Delay_ms(t);
-		}
+#include <regx52.h>
+unsigned char dem = 0;
+unsigned char dvi = 0;
+void main(){	
+	dem = 234;
+	dvi = dem%10;
+	P2 = dvi;				
+	while(1){
 	}
 }
