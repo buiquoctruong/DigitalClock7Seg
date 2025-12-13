ba30a311e29fb84c36a69b778d3c6a1b6b3e3b31
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 10:35:38 +0700

    CT02: hiệu ứng sử dụng lệnh đảo Port

--- firmware/main.c
+++ firmware/main.c
@@ -1,21 +1,18 @@
 #include <REGX52.H>
-#include ".\ThuVien\Delay.h"
-void main (){
+#include ".\ThuVien\Delay.h"	
+#define LED_PORT P0
+void main(){
 	unsigned char i;
 	unsigned int t;
 	while (1){
 		t = 500;
 		for (i = 0; i < 5; i++) {
-			P0 = 0x00; Delay_ms(t);
-			P0 = 0xFF; Delay_ms(t);
+			LED_PORT = 0x00; 	Delay_ms(t);
+			LED_PORT = ~P0; 	Delay_ms(t);
 		}
 		for (i = 0; i < 5; i++) {
-			P0 = 0x0F; Delay_ms(t);
-			P0 = 0xF0; Delay_ms(t);
-		}
-		for (i=0; i < 5;i++) {
-			P0 = 0x55; Delay_ms(t);
-			P0 = 0xAA; Delay_ms(t);
+			LED_PORT = 0xAA;	Delay_ms(t);
+			LED_PORT = ~P0; 	Delay_ms(t);
 		}
 	}
 }
