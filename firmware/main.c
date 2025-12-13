f76ab849ef4e631316d320336f4b3605f7d10ba3
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 11:03:03 +0700

    CT06: cải tiến chương trình 05

--- firmware/main.c
+++ firmware/main.c
@@ -4,19 +4,25 @@
 #define LED_P3 P3
 sbit Start = P1^0;
 sbit Stop = P1^1;
-void main() {
-	LED_P0 = 0xFF;
-	LED_P3 = 0xFF;
-	while(1) {
+void main(){
+	unsigned char i;
+	LED_P0 = 0x00;
+	LED_P3 = 0x00;
+	while(1){ 	//while(1)_1
 		if (Start == 0){
-			while(1) {
+			while(1){	//while(1)_2
 				LED_P0 = ~LED_P0;
 				LED_P3 = ~LED_P3;
-				Delay_ms(500);
-				if (Stop == 0){
-					LED_P0 = 0x00;
-					LED_P3 = 0x00;
-					break;
+				for (i=0; i<100; i++){
+					Delay_ms(10);
+					if (Stop == 0){
+						LED_P0 = 0x00;
+						LED_P3 = 0x00;
+						break;	//break_1
+					}
+				}
+				if (i != 100){
+					break; 	//break_2
 				}
 			}
 		}
