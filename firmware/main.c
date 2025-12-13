1a40cafe389795c9c63c325cf6f44dd151c9e5a8
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 10:58:37 +0700

    CT02: nút nhấn B1 điều khiển bật/tắt LED

--- firmware/main.c
+++ firmware/main.c
@@ -1,15 +1,14 @@
 #include <REGX52.H>
+#include <.\ThuVien\Delay.h>
 sbit LED = P3^0;
 sbit NutNhanB1 = P1^0;
 void main() {
 	P0 = P3 = 0;
-	LED = 0; 	
-	while (1){
-		if ( NutNhanB1 == 0) {	
-			LED = 1; 	
-		}
-		else {		
-		 	LED = 0;
+	LED = 0;
+	while (1) {
+		if (NutNhanB1 == 0) {
+			LED = !LED;
+			while (NutNhanB1 == 0); 
 		}
 	}
 }
