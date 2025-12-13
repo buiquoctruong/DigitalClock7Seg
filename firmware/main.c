cd3246f0d17c2035c4b875b369523be6673283b4
buiquoctruong <buiquoctruong@gmail.com>
2025-12-13 12:59:41 +0700

    CT02: Hiển thị 1 số trên LED 7 đoạn bằng cách gọi từ mảng

--- firmware/main.c
+++ firmware/main.c
@@ -1,17 +1,11 @@
 #include <regx52.h>
 #include ".\ThuVien\Delay.h"
+unsigned char code Code7Seg[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
 void main(){
 	while(1){
-	P2_0 = 1;	
-	P2_1 = 0;
-	P2_2 = 0;
-	P2_3 = 1;
-	P2_4 = 1;
-	P2_5 = 1;
-	P2_6 = 1;
-	P2_7 = 1;
-	Delay_ms(1000);
-	P2 = 0xA4;
-	Delay_ms(1000);
+		P2 = Code7Seg[3];
+		Delay_ms(1000);
+		P2 = Code7Seg[4];
+		Delay_ms(1000);
 	}
 }
